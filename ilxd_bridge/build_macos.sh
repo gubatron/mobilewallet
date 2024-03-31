917b3d4341894da35b67060ec789ec45c3ebf5ef
Angel Leon <gubatron@gmail.com>
2024-03-31 14:00:50 -0600

    Refactor macOS build process and clean up Objective-C integration
    This commit introduces a series of improvements and cleanups to the macOS build process for the ilxd_bridge project. Key changes include:
    
    - Enhanced script readability and maintenance by adding the set -x command to print executed commands, improving traceability.
    - Introduced rm_if_exists and build_rust_crate_archive functions to streamline the removal and building of Rust crate archives, promoting code reuse and reducing duplication.
    - Simplified the project structure by removing unnecessary Objective-C header (.h) and implementation (.m) files, as well as a C++ wrapper file, focusing on a leaner integration approach.
    - Updated the build_macos.sh script to include cleaning steps for previous build artifacts (.o, .a, .dylib files, and temporary object directories), ensuring a clean state before each build.
    - Adjusted the clang commands for compiling Objective-C code and creating shared libraries to explicitly link against the Foundation and SystemConfiguration frameworks and to include an exported symbols list. This addresses linking issues and specifies the symbols to be exported from the shared library.
    - Removed unused variables and commented-out code blocks to improve the code's readability and maintainability.
    - Updated the exported_symbols.txt file to reflect current symbol exports, ensuring consistency with the project's requirements.

--- ilxd_bridge/build_macos.sh
+++ ilxd_bridge/build_macos.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
+#print the commands being executed
+set -x
+
+rm_if_exists() {
+    if [ -f "$1" ]; then
+        rm $1
+    fi
+}
+
+build_rust_crate_archive() {
+    pushd $1
+    RUST_TARGET_ARCH=$2
+    echo ${PWD}
+    rustup target add ${RUST_TARGET_ARCH}
+    export MACOSX_DEPLOYMENT_TARGET=10.13
+    cargo build --target ${RUST_TARGET_ARCH} --release
+    popd
+}
 
 # One step build and test for the macos binaries
 # All resulting binaries for macos are in mobilewallet/ilxd_bridge/macos
 
-pushd macos/Classes
+pushd macos
+rm *.o
+rm *.a
+rm *.dylib
+rm -fr libillium_zk_objs
+rm -fr libillium_crypto_objs
 OS_NAME=macos
 MAC_OS_VERSION_MIN=10.13
 RUST_TARGET_ARCH=aarch64-apple-darwin
+ARCH=arm64
 # Set the path to the Xcode Developer directory
 XCODE_DEV_DIR=$(xcode-select --print-path)
-
 # Set the path to the macOS SDK
 MACOS_SDK_PATH=$(xcrun --sdk macosx --show-sdk-path)
 
@@ -24,72 +47,59 @@ if [ -z "$ILXD_HOME" ]; then
 fi
 
 # Make sure ILXD_HOME exists
-if [ ! -d "$ILXD_HOME" ]; then
-  echo "The folder $ILXD_HOME does not exist."
-  exit 1 # Exits the script with a status of 1 indicating an error.
+if [ ! -d "${ILXD_HOME}" ]; then
+  echo "The folder ${ILXD_HOME} does not exist."
+  exit 1
 fi
 
-# Remove any previous build of libillium_zk
-if [ -f "${ILXD_HOME}/zk/rust/target/${RUST_TARGET_ARCH}/release/libillium_zk.a" ]; then
-  rm -f ${ILXD_HOME}/zk/rust/target/${RUST_TARGET_ARCH}/release/libillium_zk.*
-fi
+rm_if_exists "${ILXD_HOME}/zk/rust/target/${RUST_TARGET_ARCH}/release/libillium_zk.a"
+rm_if_exists "${ILXD_HOME}/crypto/rust/target/${RUST_TARGET_ARCH}/release/libillium_crypto.a"
 
 # Build libillium_zk.a
-pushd ${ILXD_HOME}/zk/rust
-rustup target add aarch64-apple-darwin
-export MACOSX_DEPLOYMENT_TARGET=10.13
-# this should ideally set mmacosx-version-min=10.13 in the rust static libraries
-cargo build --target ${RUST_TARGET_ARCH} --release
+build_rust_crate_archive ${ILXD_HOME}/zk/rust ${RUST_TARGET_ARCH}
+# Build libillium_crypto.a
+#build_rust_crate_archive ${ILXD_HOME}/crypto/rust ${RUST_TARGET_ARCH}
+
+# Copy libillium_zk.a and libillium_crypto.a into our bridge's macos/ folder
+cp ${ILXD_HOME}/zk/rust/target/${RUST_TARGET_ARCH}/release/libillium_zk.a .
+#cp ${ILXD_HOME}/crypto/rust/target/${RUST_TARGET_ARCH}/release/libillium_crypto.a .
+
+# Compile the Objective-C code
+clang -c ilxd_bridge.m -o "ilxd_bridge_${OS_NAME}_${ARCH}.o" \
+    -arch ${ARCH} \
+    -isysroot "${MACOS_SDK_PATH}" \
+    -fobjc-arc \
+    -fmodules \
+    -mmacosx-version-min=${MAC_OS_VERSION_MIN}
+
+# Create the shared library
+clang -dynamiclib -o "libilxd_bridge_${OS_NAME}_${ARCH}.dylib" \
+    "ilxd_bridge_${OS_NAME}_${ARCH}.o" \
+    ${ILXD_HOME}/zk/rust/target/aarch64-apple-darwin/release/libillium_zk.a \
+    -arch ${ARCH} \
+    -isysroot "${MACOS_SDK_PATH}" \
+    -fobjc-arc \
+    -fmodules \
+    -mmacosx-version-min=${MAC_OS_VERSION_MIN} \
+    -framework Foundation \
+    -framework SystemConfiguration \
+    -lc++ \
+    -Wl,-exported_symbols_list,../exported_symbols.txt
+
+# back to ilxd_bridge/
 popd
 
-# Copy libillium_zk.a into our bridge's macos/ folder
-cp ${ILXD_HOME}/zk/rust/target/${RUST_TARGET_ARCH}/release/libillium_zk.a ..
+if [ ! -f "macos/libilxd_bridge_${OS_NAME}_${ARCH}.dylib" ]; then
+    echo "Error: libilxd_bridge_${OS_NAME}_${ARCH}.dylib not found"
+    exit 1
+fi
 
-# Set the Objective-C shared library target architecture
-ARCHS=("arm64")
-
-# Compile the Objective-C code and create the shared library for each architecture
-for ARCH in "${ARCHS[@]}"
-do
-  # Compile the Objective-C code
-  clang -c IlxdBridge.m -o "IlxdBridge_${OS_NAME}_${ARCH}.o" \
-      -arch ${ARCH} \
-      -isysroot "${MACOS_SDK_PATH}" \
-      -fobjc-arc \
-      -fmodules \
-      -mmacosx-version-min=${MAC_OS_VERSION_MIN}
-
-  # Create the shared library
-  clang -dynamiclib -o "libilxd_bridge_${OS_NAME}_${ARCH}.dylib" \
-      "IlxdBridge_${OS_NAME}_${ARCH}.o" \
-      ${ILXD_HOME}/zk/rust/target/aarch64-apple-darwin/release/libillium_zk.a \
-      -arch ${ARCH} \
-      -isysroot "${MACOS_SDK_PATH}" \
-      -fobjc-arc \
-      -fmodules \
-      -mmacosx-version-min=${MAC_OS_VERSION_MIN} \
-      -framework Foundation \
-      -lc++
-
-  # If previous versions of the libraries exist, remove them
-  if [ -f "../libilxd_bridge_${OS_NAME}_${ARCH}.dylib" ]; then
-    rm ../libilxd_bridge_${OS_NAME}_${ARCH}.dylib
-  fi
-
-  if [ -f "../IlxdBridge_${ARCH}.o" ]; then
-    rm ../IlxdBridge_${OS_NAME}_${ARCH}.o
-  fi
-
-  mv libilxd_bridge_${OS_NAME}_${ARCH}.dylib .. && echo "Shared library built successfully: libilxd_bridge_${OS_NAME}_${ARCH}.dylib, moved to ../ -> ilxd_bridge/macos"
-  mv IlxdBridge_${OS_NAME}_${ARCH}.o .. && echo "Also moved IlxdBridge_${OS_NAME}_${ARCH}.o to ../ -> ilxd_bridge/macos"
-done
-
-popd
-
-echo "./macos contents..."
+echo "ilxd_bridge/macos:"
 ls -l macos/**
 echo ""
 
 # Now build the dart component
 dart pub get
 dart run test/test_macos.dart
+
+set +x
--- ilxd_bridge/macos/ilxd_bridge.m
+++ ilxd_bridge/macos/ilxd_bridge.m
@@ -1,10 +1,9 @@
 // Copyright (c) 2024 Project Illium
 // This work is licensed under the terms of the MIT License
 // For a copy, see <https://github.com/project-illium/mobilewallet/blob/main/LICENSE>
-#include <stdint.h>
-#include <stdbool.h>
-#include <cstddef>
+#import <Foundation/Foundation.h>
 
+// libillium_zk.a rust symbols
 extern int lurk_commit(const char* expr, uint8_t* out);
 extern void load_public_params();
 extern int create_proof_ffi(const char* lurk_program, const char* private_params, const char* public_params, size_t max_steps, uint8_t* proof, size_t* proof_len, uint8_t* output_tag, uint8_t* output_val);
